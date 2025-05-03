e6e565fde70474c3c42bb8de20c0df8536751323
prabhu4302 <prabhusubbiah@gmail.com>
2025-05-03 11:59:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -44,5 +44,4 @@ def index():
 
 if __name__ == '__main__':
     os.makedirs('uploads', exist_ok=True)
-    app.run(debug=True)
-app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
